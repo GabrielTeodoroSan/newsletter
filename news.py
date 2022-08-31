69cba28a09e63c0a0a06fa6990c4329cd05ede4d
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-08-31 05:38:45 -0300

    Modificando o arquivo news para captar apenas noticias da página príncipal do g1.

--- news.py
+++ news.py
@@ -2,27 +2,26 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
-def getNews(keyWord):
-    html = urlopen(formatUrl(keyWord))
-    bs = BeautifulSoup(html, 'html.parser')
-    news = bs.find_all('div', {'class': 'widget--info__text-container'}, limit=3)
-    return formatNews(news)
+def createBs(url):
+    html = urlopen(url)
+    return BeautifulSoup(html, 'html.parser')
 
 
-def formatUrl(keyWord):
-    return f'https://g1.globo.com/busca/?q={keyWord}'
+def getNews():
+    bs = createBs('https://g1.globo.com/')
+    notices = bs.find_all('a', {'class': 'feed-post-link'}, limit=4)
+    return formatNews(notices)
 
 
 def formatNews(news):
-    notices = []
+    formatedNews = []
     for notice in news:
-        notices.append([
-            notice.find('div', {'class': 'widget--info__title'}).get_text(),
-            f"https:{notice.find('a').attrs['href']}"
+        formatedNews.append([
+            notice.text,
+            notice.attrs['href']
         ])
+    return formatedNews
 
-    return notices
 
-
-if __name__ == '__main__':
-    print(getNews('parana'))
+if __name__ == "__main__":
+    print(getNews())
