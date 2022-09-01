5822282daf3a0f58416819225ed54fa296262cb4
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-09-01 04:52:10 -0300

    Concertando o bot inserindo os intents...

--- newsletter.py
+++ newsletter.py
@@ -3,8 +3,9 @@ from news import getNews
 from discord.ext import commands
 from settings import DISCORD
 
+intents = discord.Intents.default()
 
-bot = commands.Bot('!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
