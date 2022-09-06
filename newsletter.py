0d4dcc05fc2469621605adc9421889b2197d58fd
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-09-06 17:29:37 -0300

    arrumando o bot.

--- newsletter.py
+++ newsletter.py
@@ -4,12 +4,21 @@ from discord.ext import commands
 from settings import DISCORD
 
 intents = discord.Intents.default()
+intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
     print('Tudo certo')
+
+
+@bot.command()
+async def news(ctx):
+    notices = getNews()
+    for notice in notices:
+            await ctx.send(notice[1])
+
 
 
 bot.run(DISCORD['token'])
